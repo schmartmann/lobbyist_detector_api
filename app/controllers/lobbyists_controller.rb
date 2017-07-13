3f0151ddd1ddf23c5b3e751fc46e71d0de60ef02
Stefan Hartmann <stefanhartmann@gmail.com>
2017-07-13 18:10:09 -0400

    add basic search path

--- app/controllers/lobbyists_controller.rb
+++ app/controllers/lobbyists_controller.rb
@@ -1,2 +1,11 @@
 class LobbyistsController < ApplicationController
+
+  def index
+    lobbyists = Lobbyist.all
+    render json: lobbyists
+  end
+
+  def show
+    render json: Lobbyist.search(params[:name]).length > 0 ? Lobbyist.search(params[:name]) : "nobody found"
+  end
 end
